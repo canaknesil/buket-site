612d9f4bdc111e46769659963905bcf851b57876
Can Aknesil <canaknesil@c-f900225c.034-412-73746f7.bbcust.telenor.se>
2020-06-05 00:06:20 +0200

    Separate pages for every menu item.

--- js-import/navigation.js
+++ js-import/navigation.js
@@ -1,22 +1,40 @@
+is_ana_sayfa_active = "";
+is_hakkimda_active = "";
+is_iletisim_active = "";
+
+switch(active_link_for_nav) {
+case "ana_sayfa":
+    is_ana_sayfa_active = "active";
+    break;
+case "hakkimda":
+    is_hakkimda_active = "active";
+    break;
+case "iletisim":
+    is_iletisim_active = "active";
+    break;
+default:
+    
+} 
+
 document.write(`
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
   <div class="container">
-    <a class="navbar-brand js-scroll-trigger" href="#page-top">Buket Özensoy</a>
+    <a class="navbar-brand" href="/">Buket Özensoy</a>
     <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-      Menu
+      Menü
       <i class="fas fa-bars"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item mx-0 mx-lg-1">
-          <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Çalışmalarım</a>
+          <a class="nav-link py-3 px-0 px-lg-3 rounded ${is_ana_sayfa_active}" href="/">Ana Sayfa</a>
         </li>
         <li class="nav-item mx-0 mx-lg-1">
-          <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Hakkımda</a>
+          <a class="nav-link py-3 px-0 px-lg-3 rounded ${is_hakkimda_active}" href="/hakkimda">Hakkımda</a>
         </li>
         <li class="nav-item mx-0 mx-lg-1">
-          <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">İletişim</a>
+          <a class="nav-link py-3 px-0 px-lg-3 rounded ${is_iletisim_active}" href="/iletisim">İletişim</a>
         </li>
       </ul>
     </div>
